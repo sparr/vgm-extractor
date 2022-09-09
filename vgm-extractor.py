c2371a4df414416885434085a92de9f5414c312d
Clarence "Sparr" Risher <sparr0@gmail.com>
2022-09-09 13:12:36 -0400

    Attempt to find Steam path on Windows. Based on https://github.com/sparr/vgm-extractor/pull/2

--- vgm-extractor.py
+++ vgm-extractor.py
@@ -10,9 +10,13 @@ import shutil
 import subprocess
 import vdf
 import yaml
+
 from pathlib import Path
 from sys import platform
 from zipfile import ZipFile
+
+if platform.startswith('win'):
+    import winreg
 
 arg_parser = argparse.ArgumentParser(
     formatter_class=argparse.ArgumentDefaultsHelpFormatter
@@ -60,26 +64,43 @@ if not Path(args.outputpath).is_dir():
 
 output_path = Path(args.outputpath)
 
-steam_library_paths = []
-
 if args.steamlibrarypath:
-    if not Path(args.steamlibrarypath).is_dir():
+    path = Path(args.steamlibrarypath)
+    if not path.is_dir():
         arg_parser.error("steamlibrarypath is not a directory")
-    steam_library_paths = [Path(args.steamlibrarypath)]
+    steam_library_paths = [path]
 else:
+    steam_library_paths = []
     if platform.startswith('win'):
-        pass
+        try:
+            # 64 bit windows
+            hkey = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, "SOFTWARE\WOW6432Node\Valve\Steam")
+        except:
+            try:
+                # 32 bit windows
+                hkey = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, "SOFTWARE\Valve\Steam") 
+            except:
+                raise Exception("failed to find Steam registry keys, use --steamapppath argument instead")
+        steam_install_path = Path(winreg.QueryValueEx(hkey, "InstallPath")[0])
+
     # TODO support mac, probably looking in ~/Library/Application Support/Steam
     else:
         # ~/.steam/steam is a symlink to the Steam installation directory
-        with open(Path.home() / ".steam/steam/config/libraryfolders.vdf") as vdf_file:
-            libraryfolders_vdf = vdf.parse(vdf_file)
-        for k,v in libraryfolders_vdf["libraryfolders"].items():
-            if k.isdigit(): # library folder entries have keys like "0" "1" etc
-                path = Path(v["path"])
-                if path.is_dir():
-                    steam_library_paths.append(path)
-        # TODO fallback on .steam/steam/config/config.vdf BaseInstallFolder_1
+        steam_install_path = Path.home() / ".steam/steam"
+    if not steam_install_path.is_dir():
+        raise Exception("Failed to locate Steam installation directory, use --steamlibrarypath argument instead")
+
+    with open(steam_install_path / "config/libraryfolders.vdf") as vdf_file:
+        libraryfolders_vdf = vdf.parse(vdf_file)
+    for k,v in libraryfolders_vdf["libraryfolders"].items():
+        if k.isdigit(): # library folder entries have keys like "0" "1" etc
+            path = Path(v["path"])
+            if path.is_dir():
+                steam_library_paths.append(path)
+    # TODO fallback on config/config.vdf BaseInstallFolder_1
+
+if len(steam_library_paths) == 0:
+    raise Exception("Failed to locate any Steam library directory, use --steamlibrarypath argument instead")
 
 game_data = {}
 
