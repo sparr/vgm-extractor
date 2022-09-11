165c2f627a79a78f0cba8f0a2c7868c54d7595d1
Clarence "Sparr" Risher <sparr0@gmail.com>
2022-09-11 14:53:41 -0700

    Move remaining steps into steps module.

--- src/steps.py
+++ src/steps.py
@@ -1,16 +1,20 @@
+import fnmatch
+import subprocess
+
 from pathlib import Path
+from UnityPy.enums.ClassIDType import ClassIDType
+from zipfile import ZipFile
 
 import file_util
+import unity_extract
 
 class Step():
     def __init__(self, step):
-        pass
+        self.step = step
     def execute(self):
         pass
 
 class FilespecStep(Step):
-    def __init__(self, step):
-        self.step = step
     def execute(self, config, args, gameconfig):
         if isinstance(self.step["filespec"], list):
             if args.format == "*":
@@ -50,8 +54,6 @@ class FilespecStep(Step):
                 pass
 
 class PythonStep(Step):
-    def __init__(self, step):
-        self.step = step
     def execute(self, config, args, gameconfig):
             python_output = getattr(config.gamedata[gameconfig.gamename]["python"], self.step["python"])(
                 config, args, gameconfig
@@ -63,3 +65,96 @@ class PythonStep(Step):
                 if args.verbose > 1:
                     for file in python_output.args:
                         print("  " + file)
+
+class ZipfileStep(Step):
+    def execute(self, config, args, gameconfig):
+        with ZipFile(
+            Path(gameconfig.game_folder.joinpath(self.step["zipfile"])), "r"
+        ) as zipfile:
+            found_files = False
+            zipfilespec = self.step["zipfilespec"]
+            if not zipfilespec:
+                zipfilespec = ["*"]
+            else:
+                if isinstance(zipfilespec, str):
+                    zipfilespec = [zipfilespec]
+            for spec in zipfilespec:
+                for filename in zipfile.namelist():
+                    if fnmatch.fnmatch(filename, spec):
+                        if (
+                            args.overwrite
+                            or not gameconfig.output_game_path.joinpath(filename).exists()
+                        ):
+                            found_files = True
+                            zipfile.extract(filename, path=gameconfig.output_game_path)
+                            file_util.tag(gameconfig.output_game_path / filename, gameconfig.gamename, args.albumsuffix)
+                            (gameconfig.output_game_path / filename).rename(
+                                gameconfig.output_game_path / Path(filename).name
+                            )
+                            if args.verbose > 1:
+                                print("  " + filename)
+                if found_files:
+                    break
+        # TODO: eliminate unwanted levels of folder nesting here
+
+# unxwb from https://aluigi.altervista.org/papers.htm#xbox
+# TODO: support --overwrite
+class XwbfileStep(Step):
+    def execute(self, config, args, gameconfig):
+        if args.overwrite:
+            prompt_key = "y"
+        else:
+            prompt_key = "n"
+        yes = subprocess.Popen(["yes", prompt_key], stdout=subprocess.PIPE)
+        # TODO capture output, suppress based on verbosity
+        unxwb = subprocess.run(
+            [
+                "unxwb",
+                "-d",
+                gameconfig.output_game_path,
+                "-b",
+                gameconfig.game_folder.joinpath(self.step["xsb_file"]),
+                str(self.step.get("xsb_offset", 0)),
+                gameconfig.game_folder.joinpath(self.step["xwb_file"]),
+            ],
+            stdin=yes.stdout,
+        )
+        yes.stdout.close()
+
+class AssetsfileStep(Step):
+    def execute(self, config, args, gameconfig):
+        assetsfilespec = self.step.get("assetsfilespec", None)
+        assetsexcludespec = self.step.get("assetsexcludespec", None)
+        if not isinstance(assetsfilespec, list):
+            assetsfilespec = [assetsfilespec]
+        if not isinstance(assetsexcludespec, list):
+            assetsexcludespec = [assetsexcludespec]
+        def asset_filter(obj):
+            if obj.type == ClassIDType.AudioClip:
+                if obj.m_Length and obj.m_Length < args.minduration:
+                    return False
+                if obj.name:
+                    # TODO sync with other multiple filespec behavior 
+                    for excludespec in assetsexcludespec:
+                        if excludespec and fnmatch.fnmatch(obj.name, excludespec):
+                            return False
+                    for filespec in assetsfilespec:
+                        if filespec and fnmatch.fnmatch(obj.name, filespec):
+                            # print("======", obj.name)
+                            # for k in dir(obj):
+                            #     print(k,str(getattr(obj,k))[:100])
+                            return True
+                    return False
+                return True
+            return False
+        # print(config.game_folder.joinpath(step["assetsfile"]), config.output_game_path)
+        path_id_list = unity_extract.extract_assets(
+            str(gameconfig.game_folder.joinpath(self.step["assetsfile"])),
+            gameconfig.output_game_path,
+            use_container = False,
+            append_path_id = False,
+            asset_filter = asset_filter,
+        )
+        if len(path_id_list) == 0:
+            raise "Empty unity extract"
+        # TODO collect and output filenames for verbose>1
--- src/vgm-extractor.py
+++ src/vgm-extractor.py
@@ -2,14 +2,9 @@
 
 import fnmatch
 from importlib.resources import path
-import os
 import shutil
-import subprocess
 
 from pathlib import Path
-from zipfile import ZipFile
-
-from UnityPy.enums.ClassIDType import ClassIDType
 
 import args
 import config
@@ -17,7 +12,6 @@ import file_util
 import gamedata
 import game_config
 import steps
-import unity_extract
 
 parsed_args = args.parse()
 
@@ -50,102 +44,21 @@ for game_name in configuration.games:
             steps.PythonStep(step).execute(configuration, parsed_args, game_configuration)
 
         # tag_filespec describes the output files to be tagged
-        if "tag_filespec" in step:
+        elif "tag_filespec" in step:
             # tag files from the script
             for filepath in game_configuration.output_game_path.glob(step["tag_filespec"]):
                 file_util.tag(filepath, game_name, parsed_args.albumsuffix)
 
         # zip files
-        if "zipfile" in step:
-            with ZipFile(
-                Path(game_configuration.game_folder.joinpath(step["zipfile"])), "r"
-            ) as zipfile:
-                found_files = False
-                zipfilespec = step["zipfilespec"]
-                if not zipfilespec:
-                    zipfilespec = ["*"]
-                else:
-                    if isinstance(zipfilespec, str):
-                        zipfilespec = [zipfilespec]
-                for spec in zipfilespec:
-                    for filename in zipfile.namelist():
-                        if fnmatch.fnmatch(filename, spec):
-                            if (
-                                parsed_args.overwrite
-                                or not game_configuration.output_game_path.joinpath(filename).exists()
-                            ):
-                                found_files = True
-                                zipfile.extract(filename, path=game_configuration.output_game_path)
-                                file_util.tag(game_configuration.output_game_path / filename, game_name, parsed_args.albumsuffix)
-                                (game_configuration.output_game_path / filename).rename(
-                                    game_configuration.output_game_path / Path(filename).name
-                                )
-                                if parsed_args.verbose > 1:
-                                    print("  " + filename)
-                    if found_files:
-                        break
-            # TODO: eliminate unwanted levels of folder nesting here
+        elif "zipfile" in step:
+            steps.ZipfileStep(step).execute(configuration, parsed_args, game_configuration)
 
         # xwb files
-        # unxwb from https://aluigi.altervista.org/papers.htm#xbox
-        # TODO: support --overwrite
-        if "xwb_file" in step and "xsb_file" in step and shutil.which("unxwb"):
-            if parsed_args.overwrite:
-                prompt_key = "y"
-            else:
-                prompt_key = "n"
-            yes = subprocess.Popen(["yes", prompt_key], stdout=subprocess.PIPE)
-            # TODO capture output, suppress based on verbosity
-            unxwb = subprocess.run(
-                [
-                    "unxwb",
-                    "-d",
-                    game_configuration.output_game_path,
-                    "-b",
-                    game_configuration.game_folder.joinpath(step["xsb_file"]),
-                    str(step.get("xsb_offset", 0)),
-                    game_configuration.game_folder.joinpath(step["xwb_file"]),
-                ],
-                stdin=yes.stdout,
-            )
-            yes.stdout.close()
+        elif "xwb_file" in step and "xsb_file" in step and shutil.which("unxwb"):
+            steps.XwbfileStep(step).execute(configuration, parsed_args, game_configuration)
 
         # Unity assets files
         if "assetsfile" in step:
-            assetsfilespec = step.get("assetsfilespec", None)
-            assetsexcludespec = step.get("assetsexcludespec", None)
-            if not isinstance(assetsfilespec, list):
-                assetsfilespec = [assetsfilespec]
-            if not isinstance(assetsexcludespec, list):
-                assetsexcludespec = [assetsexcludespec]
-            def asset_filter(obj):
-                if obj.type == ClassIDType.AudioClip:
-                    if obj.m_Length and obj.m_Length < parsed_args.minduration:
-                        return False
-                    if obj.name:
-                        # TODO sync with other multiple filespec behavior 
-                        for excludespec in assetsexcludespec:
-                            if excludespec and fnmatch.fnmatch(obj.name, excludespec):
-                                return False
-                        for filespec in assetsfilespec:
-                            if filespec and fnmatch.fnmatch(obj.name, filespec):
-                                # print("======", obj.name)
-                                # for k in dir(obj):
-                                #     print(k,str(getattr(obj,k))[:100])
-                                return True
-                        return False
-                    return True
-                return False
-            # print(game_configuration.game_folder.joinpath(step["assetsfile"]), game_configuration.output_game_path)
-            path_id_list = unity_extract.extract_assets(
-                str(game_configuration.game_folder.joinpath(step["assetsfile"])),
-                game_configuration.output_game_path,
-                use_container = False,
-                append_path_id = False,
-                asset_filter = asset_filter,
-            )
-            if len(path_id_list) == 0:
-                raise "Empty unity extract"
-            # TODO collect and output filenames for verbose>1
+            steps.AssetsfileStep(step).execute(configuration, parsed_args, game_configuration)
 
     file_util.remove_empty_dir_tree(game_configuration.output_game_path)
